e5bc3798015080f5727c0adb7850fd41466b7bf9
Thore Buecking <rmaptbu@ucl.ac.uk>
2015-04-10 15:25:48 +0100

    measured nmi of all template images to each other

--- LabelFusion.m
+++ LabelFusion.m
@@ -5,24 +5,30 @@ for j=0:9
     label_fusions=[];
     
     refstr=strcat('template_',num2str(j),'_brain.nii');
+    refI=strcat('template_',num2str(j),'.nii');
     ref=nifti(refstr);
     ref=numeric(ref.dat);
+    refI=nifti(refI);
+    refI=numeric(refI.dat);
     for i=0:9 %for each template
-
         %load labels
         if i~=j
             appendix=['_it',num2str(300),'_ln',num2str(3),'_lp',num2str(3),'_option',num2str(4)];
             floatimage=strcat('template_',num2str(i),'.nii');
             registered_segment=strcat('ref_',refstr(7:10),'_flo_',floatimage(1:5),floatimage(10),'_brain',appendix,'.nii');
+            registered_image=strcat('ref_',refstr(7:10),'_flo_',floatimage(1:5),floatimage(10),'_nrr',appendix,'.nii');
+%             registered_image=nifti(registered_image);
             label=nifti(registered_segment);
             label=numeric(label.dat);
+%             registered_image=numeric(registered_image.dat);
             labels=cat(4,label,labels);
+%             weight(i+1,j+1)=nmi(refI,registered_image);
         end
     end
     %majority voting
     sum_labels=sum(labels,4);
     label_fusion=zeros(size(sum_labels));
-    label_fusion(sum_labels>4)=1; %5 or more voted for that voxel
+    label_fusion(sum_labels>3)=1; %5 or more voted for that voxel
     label_fusions=cat(4,label_fusion,label_fusions); %store data
     
     %calculate dice score
